bc360e35d794277abd261f1058f63a3964e4a9ee
andrzguti22 <andrzguti@gmail.com>
2026-08-31 22:42:32 -0500

    fix: se agrega clase JobUpdate al schema

--- Backend/app/schemas/job.py
+++ Backend/app/schemas/job.py
@@ -1,9 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
 
 class JobCreate(BaseModel):
-    title: str
-    description: str
-    location: str
+    title: str = Field(..., min_length=3, max_length=100)
+    description: str = Field(..., min_length=10, max_length=1000)
+    location: str = Field(..., min_length=2, max_length=150)
     price: float
     owner_id: int
-    
+
+
+class JobUpdate(BaseModel):
+    title: str = Field(..., min_length=3, max_length=100)
+    description: str = Field(..., min_length=10, max_length=1000)
+    location: str = Field(..., min_length=2, max_length=150)
+    price: float
